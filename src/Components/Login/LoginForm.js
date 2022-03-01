c4677526e6c9f05150b4075ddaa99df394d6e555
Erick Santos <erickosantos.dev@gmail.com>
2022-03-01 19:36:59 -0300

    Creating useForm hook

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,40 +2,40 @@ import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import { Link } from 'react-router-dom';
+import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    })
-      .then((response) => {
-        console.log(response);
-        return response.json();
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+          return json;
+        });
+    }
   };
 
   return (
     <div>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="User" type="text" name="username" />
-        <Input label="Password" type="password" name="password" />
+        <Input label="User" type="text" name="username" {...username} />
+        <Input label="Password" type="password" name="password" {...password} />
         <Button>Sign in</Button>
       </form>
       <Link to="/login/create">Sign up</Link>
