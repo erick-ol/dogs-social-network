19ce707b03ab60869a8e070a527cf9f517bc19e4
Erick Santos <erickosantos.dev@gmail.com>
2022-03-01 21:12:20 -0300

    Creating UserContext

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,36 +3,18 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import { Link } from 'react-router-dom';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST, USER_GET } from '../../api';
+import { UserContext } from '../../Context/UserContext';
 
 const LoginForm = () => {
+  const { userLogin } = React.useContext(UserContext);
   const username = useForm();
   const password = useForm();
 
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) getUser(token);
-  }, []);
-
-  const getUser = async (token) => {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  };
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   };
 
