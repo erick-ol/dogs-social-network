f1bed51f20ef4d9cb0d68de61a1984a2fe7e7eef
Erick Santos <erickosantos.dev@gmail.com>
2022-03-05 16:47:11 -0300

    Styles Login Background

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -5,20 +5,22 @@ import LoginCreate from './LoginCreate';
 import LoginForm from './LoginForm';
 import LoginPasswdLost from './LoginPasswdLost';
 import LoginPasswdReset from './LoginPasswdReset';
-
+import styles from './css/Login.module.css';
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/account" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LoginPasswdLost />} />
-        <Route path="reset" element={<LoginPasswdReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LoginPasswdLost />} />
+          <Route path="reset" element={<LoginPasswdReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
