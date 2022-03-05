81c56dd010d4d2afc1fdd09b7a1e5fc189419a19
Erick Santos <erickosantos.dev@gmail.com>
2022-03-05 17:11:53 -0300

    Sign up page

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../api';
+import { UserContext } from '../../Context/UserContext';
 
 const LoginCreate = () => {
-  return <div>Register</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  };
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="User" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Password" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
