09e8a0e1923d5bf5f03814055346e26a628c1fcd
Erick Santos <erickosantos.dev@gmail.com>
2022-04-17 16:23:44 -0300

    feat: photo post component

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,7 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Invalid email.',
   },
+  number: { regex: /^\d+$/, message: 'Is only allowed numbers.' },
 };
 
 const useForm = (type) => {
