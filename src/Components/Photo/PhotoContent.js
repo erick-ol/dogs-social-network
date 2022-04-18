181bb9512c4e63a626c4a7037975c56bff917e78
Erick Santos <erickosantos.dev@gmail.com>
2022-04-18 12:11:47 -0300

    feat: dele button and photo skeleton

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../Context/UserContext';
+import Image from '../Helper/Image';
 import styles from './css/PhotoContent.module.css';
 import PhotoComments from './PhotoComments';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <p className={styles.author}>
-          <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+          {user.data && user.data.username === photo.author ? (
+            <PhotoDelete id={photo.id} />
+          ) : (
+            <Link to={`/profile/${photo.author}`}>@{photo.author}</Link>
+          )}
           <span className={styles.views}>{photo.acessos}</span>
         </p>
         <h1 className="title">
