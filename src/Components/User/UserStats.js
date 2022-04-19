b4bbebb5b359b48e4e8d1c425fe107759c0720dc
Erick Santos <erickosantos.dev@gmail.com>
2022-04-18 23:48:14 -0300

    feat: adding stats with victory lib

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const token = window.localStorage.getItem('token');
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    };
+    getData();
+  }, [request]);
+
   return (
     <div>
       <Head
         title="Photos Stats"
         description="Page that you can see the photos' stats."
       />
+      {loading && <Loading />}
+      {error && <Error error={error} />}
+      {data && <UserStatsGraphs data={data} />}
     </div>
   );
 };
